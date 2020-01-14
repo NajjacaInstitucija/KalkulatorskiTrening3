f23e647f0f5281a36db0f6af069a324109b839e3
NajjacaInstitucija <58697005+NajjacaInstitucija@users.noreply.github.com>
2020-01-14 15:59:20 +0100

    Add files via upload

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -41,15 +41,38 @@
             this.label3 = new System.Windows.Forms.Label();
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.database1DataSet = new WindowsFormsApp1.Database1DataSet();
-            this.profesoriIKolegijiBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.profesoriIKolegijiTableAdapter = new WindowsFormsApp1.Database1DataSetTableAdapters.profesoriIKolegijiTableAdapter();
             this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.imeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.kolegijDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.profesoriIKolegijiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet = new WindowsFormsApp1.Database1DataSet();
+            this.profesoriIKolegijiTableAdapter = new WindowsFormsApp1.Database1DataSetTableAdapters.profesoriIKolegijiTableAdapter();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.database2DataSet = new WindowsFormsApp1.Database2DataSet();
+            this.studentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.studentTableAdapter = new WindowsFormsApp1.Database2DataSetTableAdapters.StudentTableAdapter();
+            this.iDDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.imeDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.prezimeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.profesoriIKolegijiBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // listBox1
@@ -81,7 +104,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(140, 218);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.Size = new System.Drawing.Size(121, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "Dodaj kolegij";
             this.button1.UseVisualStyleBackColor = true;
@@ -146,7 +169,7 @@
             this.listBox2.FormattingEnabled = true;
             this.listBox2.Location = new System.Drawing.Point(324, 146);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(251, 82);
+            this.listBox2.Size = new System.Drawing.Size(161, 82);
             this.listBox2.TabIndex = 11;
             // 
             // dataGridView1
@@ -158,24 +181,10 @@
             this.imeDataGridViewTextBoxColumn,
             this.kolegijDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.profesoriIKolegijiBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(365, 284);
+            this.dataGridView1.Location = new System.Drawing.Point(273, 258);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(345, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(345, 164);
             this.dataGridView1.TabIndex = 12;
-            // 
-            // database1DataSet
-            // 
-            this.database1DataSet.DataSetName = "Database1DataSet";
-            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // profesoriIKolegijiBindingSource
-            // 
-            this.profesoriIKolegijiBindingSource.DataMember = "profesoriIKolegiji";
-            this.profesoriIKolegijiBindingSource.DataSource = this.database1DataSet;
-            // 
-            // profesoriIKolegijiTableAdapter
-            // 
-            this.profesoriIKolegijiTableAdapter.ClearBeforeFill = true;
             // 
             // iDDataGridViewTextBoxColumn
             // 
@@ -195,11 +204,199 @@
             this.kolegijDataGridViewTextBoxColumn.HeaderText = "Kolegij";
             this.kolegijDataGridViewTextBoxColumn.Name = "kolegijDataGridViewTextBoxColumn";
             // 
+            // profesoriIKolegijiBindingSource
+            // 
+            this.profesoriIKolegijiBindingSource.DataMember = "profesoriIKolegiji";
+            this.profesoriIKolegijiBindingSource.DataSource = this.database1DataSet;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // profesoriIKolegijiTableAdapter
+            // 
+            this.profesoriIKolegijiTableAdapter.ClearBeforeFill = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(514, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Novi student Ime: ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(613, 44);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(613, 114);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Dodaj studenta";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(1007, 22);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(172, 219);
+            this.treeView1.TabIndex = 16;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(799, 78);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(187, 21);
+            this.comboBox2.TabIndex = 17;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(749, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Ocjena: ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(799, 129);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(173, 20);
+            this.numericUpDown1.TabIndex = 19;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(799, 164);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(173, 20);
+            this.dateTimePicker1.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(749, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Datum: ";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(785, 205);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(187, 23);
+            this.button5.TabIndex = 22;
+            this.button5.Text = "Spremi";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(494, 82);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Novi student Prezime: ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(613, 79);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(122, 20);
+            this.textBox4.TabIndex = 24;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn1,
+            this.imeDataGridViewTextBoxColumn1,
+            this.prezimeDataGridViewTextBoxColumn});
+            this.dataGridView2.DataSource = this.studentBindingSource;
+            this.dataGridView2.Location = new System.Drawing.Point(705, 258);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(344, 164);
+            this.dataGridView2.TabIndex = 25;
+            // 
+            // database2DataSet
+            // 
+            this.database2DataSet.DataSetName = "Database2DataSet";
+            this.database2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // studentBindingSource
+            // 
+            this.studentBindingSource.DataMember = "Student";
+            this.studentBindingSource.DataSource = this.database2DataSet;
+            // 
+            // studentTableAdapter
+            // 
+            this.studentTableAdapter.ClearBeforeFill = true;
+            // 
+            // iDDataGridViewTextBoxColumn1
+            // 
+            this.iDDataGridViewTextBoxColumn1.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn1.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn1.Name = "iDDataGridViewTextBoxColumn1";
+            // 
+            // imeDataGridViewTextBoxColumn1
+            // 
+            this.imeDataGridViewTextBoxColumn1.DataPropertyName = "Ime";
+            this.imeDataGridViewTextBoxColumn1.HeaderText = "Ime";
+            this.imeDataGridViewTextBoxColumn1.Name = "imeDataGridViewTextBoxColumn1";
+            // 
+            // prezimeDataGridViewTextBoxColumn
+            // 
+            this.prezimeDataGridViewTextBoxColumn.DataPropertyName = "Prezime";
+            this.prezimeDataGridViewTextBoxColumn.HeaderText = "Prezime";
+            this.prezimeDataGridViewTextBoxColumn.Name = "prezimeDataGridViewTextBoxColumn";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1207, 450);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.label3);
@@ -216,8 +413,12 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.profesoriIKolegijiBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -243,6 +444,25 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn imeDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn kolegijDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private Database2DataSet database2DataSet;
+        private System.Windows.Forms.BindingSource studentBindingSource;
+        private Database2DataSetTableAdapters.StudentTableAdapter studentTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn imeDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn prezimeDataGridViewTextBoxColumn;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -11,11 +11,16 @@ using System.Data.OleDb;
 
 namespace WindowsFormsApp1
 {
+
+   
     public partial class Form1 : Form
     {
         Dictionary<string, List<string>> profesoriIKolegiji = new Dictionary<string, List<string>>();
 
+        List<Student> studenti = new List<Student>();
+
         OleDbConnection connection = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\student\Documents\Database1.mdb");
+        OleDbConnection connStudent = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\student\Documents\Database2.mdb");
         public Form1()
         {
             InitializeComponent();
@@ -84,6 +89,8 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'database2DataSet.Student' table. You can move, or remove it, as needed.
+            this.studentTableAdapter.Fill(this.database2DataSet.Student);
             // TODO: This line of code loads data into the 'database1DataSet.profesoriIKolegiji' table. You can move, or remove it, as needed.
             this.profesoriIKolegijiTableAdapter.Fill(this.database1DataSet.profesoriIKolegiji);
 
@@ -114,9 +121,101 @@ namespace WindowsFormsApp1
             reader.Close();
             connection.Close();
 
-            
+            OleDbCommand commandStudent = new OleDbCommand("SELECT Ime, Prezime from Student", connStudent);
+            connStudent.Open();
+            OleDbDataReader studentReader = commandStudent.ExecuteReader();
+
+            while(studentReader.Read())
+            {
+                if (!comboBox2.Items.Contains(studentReader.GetString(0) + " " + studentReader.GetString(1)))
+                    comboBox2.Items.Add(studentReader.GetString(0) + " " + studentReader.GetString(1));
+            }
+
+            studentReader.Close();
+            connStudent.Close();
 
 
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text != "" && textBox4.Text != "")
+            {
+                comboBox2.Items.Add(textBox3.Text + " " + textBox4.Text);
+
+                Student s = new Student(textBox3.Text, textBox4.Text);
+                studenti.Add(s);
+
+                OleDbCommand cmd = new OleDbCommand();
+
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT INTO Student (Ime, Prezime)" + "VALUES (@Ime, @Prezime)";
+                cmd.Parameters.AddWithValue("@Ime", s.Ime);
+                cmd.Parameters.AddWithValue("@Kolegij", s.Prezime);
+
+                cmd.Connection = connStudent;
+                connStudent.Open();
+
+                cmd.ExecuteNonQuery();
+                connStudent.Close();
+
+                this.studentTableAdapter.Fill(this.database2DataSet.Student);
+
+
+            }
+
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            treeView1.Nodes.Clear();
+            string student = comboBox2.Text;
+            // TREEVIEW
+
+            treeView1.BeginUpdate();
+
+            //Dictionary<string, List<string>>.KeyCollection profesori = profesoriIKolegiji.Keys;
+            int i = 0;
+            foreach (KeyValuePair<string, List<string>> kvp in profesoriIKolegiji)
+            {
+                treeView1.Nodes.Add(kvp.Key);
+                foreach(string kolegij in kvp.Value)
+                {
+                    treeView1.Nodes[i].Nodes.Add(kolegij);
+                }
+                i++;
+                
+            }
+            treeView1.EndUpdate();
+        }
     }
+
+
+    class Student
+    {
+        string ime;
+        string prezime;
+
+        public Student(string ime, string prezime)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+        }
+
+        public string Ime
+        {
+            get { return this.ime; }
+            set { this.ime = value; }
+        }
+
+        public string Prezime
+        {
+            get { return this.prezime; }
+            set { this.prezime = value; }
+        }
+    }
+
 }
