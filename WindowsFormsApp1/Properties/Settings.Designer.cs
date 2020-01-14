fa1b636f4126026214f96a31a796a4548552a386
NajjacaInstitucija <58697005+NajjacaInstitucija@users.noreply.github.com>
2020-01-14 15:57:59 +0100

    Add files via upload

--- WindowsFormsApp1/Properties/Settings.Designer.cs
+++ WindowsFormsApp1/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace WindowsFormsApp1.Properties {
                 return ((string)(this["Database1ConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\student\\Documents\\Database2" +
+            ".mdb")]
+        public string Database2ConnectionString {
+            get {
+                return ((string)(this["Database2ConnectionString"]));
+            }
+        }
     }
 }
